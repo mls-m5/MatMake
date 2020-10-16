1fb11136d402a32705eb8906d8925f122be92dfd
Mattias Lasersköld <mattias.l.sk@gmail.com>
2020-10-16 14:59:42 +0200

    Remove unused files

--- tests/test2.cpp
+++
@@ -1,8 +0,0 @@
-/*
- * test2.cpp
- *
- *  Created on: 13 mar 2015
- *      Author: Mattias Larsson Sköld
- */
-
-#include "test2.h"
--- tests/test2.h
+++
@@ -1,8 +0,0 @@
-/*
- * test2.h
- *
- *  Created on: 13 mar 2015
- *      Author: Mattias Larsson Sköld
- */
-
-#pragma once
